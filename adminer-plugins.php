ed3b7169bc5326ac4f43d0ee2ca07b2baa33bd7f
Ozan Cırık <263227724+ozan-crk@users.noreply.github.com>
2026-04-28 01:05:50 +0300

    Update adminer-plugins.php

--- adminer-plugins.php
+++ adminer-plugins.php
@@ -7,17 +7,23 @@ require_once('plugins/tables-filter.php');
 require_once('plugins/json-column.php');
 require_once('plugins/imagefields.php');
 require_once('plugins/readable-dates.php');
+require_once('plugins/theme-switcher.php');
 
 // ENV üzerinden sunucu listesini al (Format: Etiket1=host1,Etiket2=host2)
 $servers = getenv('ADMINER_SERVERS');
 $server_list = [];
+
 if ($servers) {
     foreach (explode(',', $servers) as $s) {
         $parts = explode('=', $s);
         if (count($parts) == 2) {
-            $server_list[$parts[0]] = $parts[1];
-        } else {
-            $server_list[$s] = $s;
+            $label = $parts[0];
+            $host = $parts[1];
+            // MySQL/MariaDB için driver 'server' olarak tanımlanır
+            $server_list[$label] = [
+                "server" => $host,
+                "driver" => "server"
+            ];
         }
     }
 }
@@ -28,4 +34,5 @@ return [
     new AdminerJsonColumn(),
     new AdminerImagefields(),
     new AdminerReadableDates(),
+    new AdminerThemeSwitcher(),
 ];

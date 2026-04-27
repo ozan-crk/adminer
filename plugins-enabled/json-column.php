1ca036d754771c65c4ba6eb99694caabfe47f972
Ozan Cırık <263227724+ozan-crk@users.noreply.github.com>
2026-04-28 01:26:02 +0300

    Update json-column.php

--- plugins-enabled/json-column.php
+++ plugins-enabled/json-column.php
@@ -1,1 +1,2 @@
-<?php require_once('plugins/plugin.php'); require_once('plugins/json-column.php'); return new AdminerJsonColumn();
+<?php require_once('plugins/json-column.php');
+return new AdminerJsonColumn();

0113c759f4905662e045dbb7bdd3a7075e271c90
Ozan Cırık <263227724+ozan-crk@users.noreply.github.com>
2026-04-28 01:24:47 +0300

    Update imagefields.php

--- plugins-enabled/imagefields.php
+++ plugins-enabled/imagefields.php
@@ -1,1 +1,2 @@
-<?php require_once('plugins/plugin.php'); require_once('plugins/imagefields.php'); return new AdminerImagefields();
+<?php require_once('plugins/imagefields.php');
+return new AdminerImagefields();
